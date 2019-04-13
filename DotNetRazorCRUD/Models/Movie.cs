7b2df6d77e379822021701ac79aeeae252428364
Kamil Wlazło <kamil119988@wp.pl>
2019-04-13 17:46:41 +0200

    One commit to rule them all

--- DotNetRazorCRUD/Models/Movie.cs
+++ DotNetRazorCRUD/Models/Movie.cs
@@ -21,9 +21,7 @@ namespace DotNetRazorCRUD.Models
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
     }
 }
